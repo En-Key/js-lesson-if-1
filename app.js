2aadde7d6a57d1ec1116e6e1e060505b49bb513d
Alexey Knyazev <bsagdiev74@gmail.com>
2022-11-21 18:59:29 +0300

    Конструкция switch-case

--- app.js
+++ app.js
@@ -1,40 +1,16 @@
-console.log('Привет');
-if (5>4) {
-    console.log("5 больше 4");
+const typeUser = 'admin'; // user, guest, bot
+
+switch (typeUser) {
+    case 'admin':
+        console.log('Вы администратор');
+        break;
+    case 'user':
+        console.log('Вы пользователь');
+        break;
+    case 'bot':
+        console.log('Вы бот');
+        break;
+    default:
+        console.log('Вы непонятно кто');       
 }
-
-let age="";
-age= +age;
-
-if (!isNaN(age)) {
-    console.log('Введите корректные данные')
-}
-else {
-    const gender='Муж';
-if (age>18) {
-    console.log("Вам больше 18");
-}
-else if (age<18) {
-    console.log("Вам меньше 18");
-} 
-else {
-    console.log("Вам 18");
-} 
-
-if(gender.toLocaleLowerCase()==='муж') {
-    console.log('Вам налево');
-}
-if(gender.toLocaleLowerCase()==='жен') {
-    console.log('Вам направо');
-}
-}
-
-const isAdmin = false;
-
-const typeUserString = isAdmin ? 'Администратор' : 'Пользователь';
-
-console.log ('Добро пожаловать: ${typeUserString}');
-
-
-
 
