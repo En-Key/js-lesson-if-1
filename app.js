891adb40a5571404ff0082553c09a92c66192f52
Alexey Knyazev <bsagdiev74@gmail.com>
2022-11-21 15:55:14 +0300

    Условия If

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log('Привет');
+if (5>4) {
+    console.log("5 больше 4");
+}
+
+const age=15;
+const gender='Муж';
+if (age>18) {
+    console.log("Вам больше 18")
+}
+if(gender.toLocaleLowerCase()==='муж') {
+    console.log('Вам налево');
+}
+if(gender.toLocaleLowerCase()==='жен') {
+    console.log('Вам направо');
+}
+
