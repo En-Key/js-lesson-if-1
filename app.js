89cd74c0df882a7d63272ea891827ec164124160
Alexey Knyazev <bsagdiev74@gmail.com>
2022-11-21 16:01:32 +0300

    Добавили else в условие проверки возраста

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ if (5>4) {
     console.log("5 больше 4");
 }
 
-const age=15;
+const age=19;
 const gender='Муж';
 if (age>18) {
     console.log("Вам больше 18")
 }
+else {
+    console.log("Вам меньше 18")
+} 
+
+
 if(gender.toLocaleLowerCase()==='муж') {
     console.log('Вам налево');
 }
